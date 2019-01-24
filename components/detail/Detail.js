eeaeb48ffb62b832385c2ab005939c1d4bf42032
Jon Madden <notjonmadden@Jons-MacBook-Pro.local>
2019-01-24 11:45:05 -0500

    adds launch prop and renders launch details if available

--- components/detail/Detail.js
+++ components/detail/Detail.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, View, Text } from 'react-native';
+import { observer } from 'mobx-react';
 
+@observer
 export default class Detail extends Component {
     render() {
+        const { launch } = this.props;
+        let text = "Select a Launch to view details";
+
+        if (launch) {
+            text = launch.details || "No details available yet... Check back later!";
+        }
+
         return (
             <View style={{...this.props.style, ...styles.container}}>
-                <Text>Details over here</Text>
+                <Text>{text}</Text>
             </View>
         );
     }
