39d2476555c33538205aa802e90fc5e78135d9b0
Jon Madden <notjonmadden@Jons-MacBook-Pro.local>
2019-01-24 16:33:19 -0500

    add search and filter

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
 import { ScreenOrientation } from 'expo';
 import { observable } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer, componentByNodeRegistry } from 'mobx-react';
 import fetch from 'cross-fetch';
 import List from './components/list';
 import Detail from './components/detail';
@@ -11,7 +11,9 @@ const { LANDSCAPE } = ScreenOrientation.Orientation;
 const appState = observable({
   launches: [],
   selectedLaunch: null,
-  rocketImages: {}
+  rocketImages: {},
+  searchText: '',
+  sort: 'desc'
 });
 
 async function getLaunchDetail(launch) {
@@ -41,13 +43,28 @@ export default class App extends React.Component {
       .then(rs => appState.rocketImages = rs.reduce((o, r) => ({ ...o, [r.rocket]: r.imageUrl })));
   }
 
+  _flipSort() {
+    const newSort = appState.sort === 'asc' ? 'desc' : 'asc';
+
+    appState.sort = newSort;
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.list}>
+          <View>
+            <TextInput
+              style={styles.search}
+              placeholder="Filter..."
+              onChangeText={t => appState.searchText = t} />
+            <Text onPress={this._flipSort}>Launch Date {(appState.sort === 'asc' ? '⬇' : '⬆')}</Text>
+          </View>
           <List
             launches={appState.launches}
             images={appState.rocketImages}
+            filter={appState.searchText}
+            sort={appState.sort}
             selectedLaunch={appState.selectedLaunch}
             onLaunchPress={async l => appState.selectedLaunch = await getLaunchDetail(l)}
           />
@@ -71,5 +88,12 @@ const styles = StyleSheet.create({
   },
   detail: {
     flex: 2
+  },
+  search: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginVertical: 8,
+    borderRadius: 2,
+    width: 256
   }
 });
--- components/list/List.js
+++ components/list/List.js
@@ -2,13 +2,48 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, ScrollView, View, Text } from 'react-native';
 import ListItem from './ListItem';
 import { observer } from 'mobx-react';
+import moment from 'moment';
+
+function getPessimisticLaunchDate(launch) {
+  if (!launch.is_tentative) {
+    return moment.utc(launch.launch_date_utc);
+  }
+
+  return moment.utc(launch.launch_date_utc).endOf(launch.tentative_max_precision);
+}
+
+const comparitors = {
+  desc(a, b) {
+    const dateDiff = getPessimisticLaunchDate(a).diff(getPessimisticLaunchDate(b));
+
+    if (dateDiff !== 0) {
+      return dateDiff;
+    }
+
+    return a.flight_number - b.flight_number;
+  },
+  asc(a, b) {
+    const dateDiff = getPessimisticLaunchDate(b).diff(getPessimisticLaunchDate(a));
+
+    if (dateDiff !== 0) {
+      return dateDiff;
+    }
+
+    return b.flight_number - a.flight_number;
+  }
+};
 
 @observer
 export default class List extends Component {
-  constructor(props = {
-    launches: []
-  }) {
+  constructor(props) {
     super(props);
+  }
+
+  _passesFilter(launch) {
+    const filters = this.props.filter.split(' ').map(w => new RegExp(w, 'i'));
+    const test = s => filters.some(f => f.test(s));
+    return test(launch.mission_name)
+        || test(launch.launch_site.site_name_long);
   }
 
   _getThumbnail(name) {
@@ -22,7 +57,15 @@ export default class List extends Component {
   }
 
   render() {
-    const { launches, selectedLaunch } = this.props;
+    const { launches, selectedLaunch, sort } = this.props;
+    const renderItem = l => (
+      <ListItem
+        onPress={() => this.props.onLaunchPress(l)}
+        key={l.flight_number}
+        launch={l}
+        selected={selectedLaunch && selectedLaunch.flight_number === l.flight_number}
+        thumbnail={this._getThumbnail(l.rocket.rocket_name)} />
+    );
 
     if (launches.length === 0) {
       return <View style={styles.loading}><Text>Loading...</Text></View>;
@@ -30,13 +73,7 @@ export default class List extends Component {
 
     return (
       <ScrollView>
-        {launches.map(l => (
-          <ListItem
-            onPress={() => this.props.onLaunchPress(l)}
-            key={l.flight_number}
-            launch={l}
-            selected={selectedLaunch && selectedLaunch.flight_number === l.flight_number}
-            thumbnail={this._getThumbnail(l.rocket.rocket_name)} />))}
+        {launches.filter(l => this._passesFilter(l)).sort(comparitors[sort]).map(renderItem)}
       </ScrollView>
     );
   }
