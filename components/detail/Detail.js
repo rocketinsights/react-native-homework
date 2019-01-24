ceee3cbeaf2ba70783e8cc6cdf8983d5234f505b
Jon Madden <notjonmadden@Jons-MacBook-Pro.local>
2019-01-24 14:20:24 -0500

    set map to display launch site

--- components/detail/Detail.js
+++ components/detail/Detail.js
@@ -7,22 +7,31 @@ import { observer } from 'mobx-react';
 export default class Detail extends Component {
     render() {
         const { launch } = this.props;
-        let text = "Select a Launch to view details";
 
-        if (launch) {
-            text = launch.details || "No details available yet... Check back later!";
+        if (!launch) {
+            return <Text>Select a Launch to view details</Text>;
         }
+
+        const text =  launch.details || "No details available yet... Check back later!";
+        const {
+            launch_site: {
+                location: {
+                    latitude,
+                    longitude
+                }
+            }
+        } = launch;
 
         return (
             <View style={styles.container}>
                 <View style={styles.mapContainer}>
                     <MapView
                         style={styles.map}
-                        initialRegion={{
-                            latitude: 42.8110228,
-                            longitude: -70.8701378,
-                            latitudeDelta: .00005,
-                            longitudeDelta: .00005
+                        region={{
+                            latitude,
+                            longitude,
+                            latitudeDelta: 10,
+                            longitudeDelta: 10
                         }}
                     />
                 </View>
