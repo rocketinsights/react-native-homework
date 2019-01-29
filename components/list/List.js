60f46f19ec244685220544c2f7f3553b7637ed0c
Jon Madden <notjonmadden@JM.local>
2019-01-29 13:06:57 -0500

    uses injected AppStore in ListItem

--- components/list/List.js
+++ components/list/List.js
@@ -6,35 +6,16 @@ import { observer, inject } from 'mobx-react';
 @inject('appStore')
 @observer
 export default class List extends Component {
-  _getThumbnail(name) {
-    const uri = this.props.appStore.rocketImages[name];
-
-    if (uri) {
-      return { uri };
-    } else {
-      return require('../../assets/rocketinsights_logo_mark.png');
-    }
-  }
 
   render() {
-    const { appStore, appStore: { launches, selectedLaunch } } = this.props;
-
-    const renderItem = l => (
-      <ListItem
-        onPress={() => appStore.selectLaunch(l)}
-        key={l.flight_number}
-        launch={l}
-        selected={selectedLaunch && selectedLaunch.flight_number === l.flight_number}
-        thumbnail={this._getThumbnail(l.rocket.rocket_name)}
-      />
-    );
+    const { appStore: { launches, selectedLaunch } } = this.props;
 
     if (launches.length === 0) {
       return <View style={styles.loading}><Text>Loading...</Text></View>;
     }
 
     return (
-      <ScrollView>{launches.map(renderItem)}</ScrollView>
+      <ScrollView>{launches.map(l => <ListItem key={l.flight_number} launch={l} />)}</ScrollView>
     );
   }
 }
--- components/list/ListItem.js
+++ components/list/ListItem.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, Image, View, TouchableHighlight } from 'react-native';
+import { inject, observer } from 'mobx-react';
 
+@inject('appStore')
+@observer
 export default class ListItem extends Component {
+    _getThumbnail(name) {
+        const uri = this.props.appStore.rocketImages[name];
+
+        if (uri) {
+            return { uri };
+        } else {
+            return require('../../assets/rocketinsights_logo_mark.png');
+        }
+    }
 
     render() {
         const launch = this.props.launch;
-        const { 
+        const thumbnail = this._getThumbnail(launch.rocket.rocket_name);
+        const selected = this.props.appStore.selectedLaunch
+            && this.props.appStore.selectedLaunch.flight_number === launch.flight_number;
+        const {
             mission_name,
             flight_number,
             launch_site: {
@@ -14,14 +29,14 @@ export default class ListItem extends Component {
         } = launch;
         let style = styles.container;
 
-        if (this.props.selected) {
+        if (selected) {
             style = { ...style, ...styles.selected };
         }
 
         return (
-            <TouchableHighlight underlayColor="grey" onPress={this.props.onPress}>
+            <TouchableHighlight underlayColor="grey" onPress={() => this.props.appStore.selectLaunch(launch)}>
                 <View style={style}>
-                    <Image style={styles.thumbnail} source={this.props.thumbnail} />
+                    <Image style={styles.thumbnail} source={thumbnail} />
                     <View style={styles.launchInfo}>
                         <Text style={styles.title}>
                             {mission_name}
