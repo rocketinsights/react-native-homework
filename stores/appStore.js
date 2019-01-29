562f3bd0958a58d7b6f9cebe281da3364d2abdc3
Jon Madden <notjonmadden@JM.local>
2019-01-29 12:53:56 -0500

    adds computed launches property which is filtered and sorted as needed

--- stores/appStore.js
+++ stores/appStore.js
@@ -1,18 +1,56 @@
-import { observable, action } from "mobx";
+import { observable, action, computed } from "mobx";
+import moment from 'moment';
 
 const LAUNCHES_URL = 'https://api.spacexdata.com/v3/launches/upcoming';
 const ROCKETS_URL = 'https://api.spacexdata.com/v3/rockets';
 const LAUNCHPADS_URL = 'https://api.spacexdata.com/v3/launchpads/';
 
+function getPessimisticLaunchDate(launch) {
+    if (!launch.is_tentative) {
+        return moment.utc(launch.launch_date_utc);
+    }
+
+    return moment.utc(launch.launch_date_utc).endOf(launch.tentative_max_precision);
+}
+
+function launchComparitor(sort) {
+    return (a, b) => {
+        const first = sort === 'desc' ? a : b;
+        const second = first === a ? b : a;
+        const dateDiff = getPessimisticLaunchDate(first).diff(getPessimisticLaunchDate(second));
+
+        if (dateDiff !== 0) {
+            return dateDiff;
+        }
+
+        return first.flight_number - second.flight_number;
+    };
+}
+
+function launchFilter(searchText) {
+    return launch => {
+        const filters = searchText.split(' ').map(w => new RegExp(w, 'i'));
+        const test = s => filters.some(f => f.test(s));
+        return test(launch.mission_name)
+            || test(launch.launch_site.site_name_long);
+    };
+}
+
 export default class AppStore {
-    @observable launches = [];
+    @observable _launches = [];
     @observable selectedLaunch = null;
     @observable rocketImages = {};
     @observable searchText = '';
     @observable sort = 'desc';
 
+    @computed get launches() {
+        return this._launches
+            .filter(launchFilter(this.searchText))
+            .sort(launchComparitor(this.sort));
+    }
+
     @action async loadAppState() {
-        this.launches = await fetch(LAUNCHES_URL)
+        this._launches = await fetch(LAUNCHES_URL)
             .then(r => r.json());
 
         this.rocketImages = await fetch(ROCKETS_URL)
@@ -31,7 +69,7 @@ export default class AppStore {
     @action async selectLaunch(launch) {
         const { launch_site: { site_id: launchpadId } } = launch;
         const launchpad = await fetch(`${LAUNCHPADS_URL}${launchpadId}`).then(r => r.json());
-      
+
         this.selectedLaunch = { ...launch, ...{ launch_site: launchpad } };
     }
 }
