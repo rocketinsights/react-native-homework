6a68357f08032eb1bb852c5c9293c96831da6e0c
Jon Madden <notjonmadden@JM.local>
2019-01-29 12:56:28 -0500

    removes unused code

--- components/list/List.js
+++ components/list/List.js
@@ -2,29 +2,6 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, ScrollView, View, Text } from 'react-native';
 import ListItem from './ListItem';
 import { observer, inject } from 'mobx-react';
-import moment from 'moment';
-
-
-const comparitors = {
-  desc(a, b) {
-    const dateDiff = getPessimisticLaunchDate(a).diff(getPessimisticLaunchDate(b));
-
-    if (dateDiff !== 0) {
-      return dateDiff;
-    }
-
-    return a.flight_number - b.flight_number;
-  },
-  asc(a, b) {
-    const dateDiff = getPessimisticLaunchDate(b).diff(getPessimisticLaunchDate(a));
-
-    if (dateDiff !== 0) {
-      return dateDiff;
-    }
-
-    return b.flight_number - a.flight_number;
-  }
-};
 
 @inject('appStore')
 @observer
@@ -48,7 +25,8 @@ export default class List extends Component {
         key={l.flight_number}
         launch={l}
         selected={selectedLaunch && selectedLaunch.flight_number === l.flight_number}
-        thumbnail={this._getThumbnail(l.rocket.rocket_name)} />
+        thumbnail={this._getThumbnail(l.rocket.rocket_name)}
+      />
     );
 
     if (launches.length === 0) {
